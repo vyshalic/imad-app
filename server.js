c6f9d053129760e0f18080be30b12363c8c2bee7
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-14 20:48:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req, res){
+   res.send('Article one requested and will sonn be served'); 
+});
+app.get('/article-two',function(req, res){
+   res.send('Article two requested and will sonn be served'); 
+});
+app.get('/article-three',function(req, res){
+   res.send('Article three requested and will sonn be served'); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
