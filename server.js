28eb07c4e56cef558793c449ae461b6c0f975af7
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-20 13:09:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ var articles= {
 function createTemplate(data){
     var title=data.title;
     var heading=data.heading;
-    var date=title.date;
-    var content=title.content;
+    var date=data.date;
+    var content=data.content;
     
     var htmlTemplate=`
         <html>
