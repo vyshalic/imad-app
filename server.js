2cae92f0d73bb5f7cc8777d012b87b3e57d5d6a5
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-20 20:22:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.get('/:articleName',function(req, res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
-var counter=0;
+var counter = 0;
 app.get('/counter', function(req,res) {
-   counter=counter+1;
+   counter = counter + 1;
    res.send(counter.toString());
 });
 
