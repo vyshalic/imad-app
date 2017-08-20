6cd4948137cadde235334dbe15ef07d81b5eccd4
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-20 14:30:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById("main-content");
 element.innerHTML="new value";
 var image=document.getElementById("madi");
+marginLeft=0;
 image.onclick=function(){
-  image.style.marginLeft='100px';  
+  function moveRight(){
+      marginLeft=marginLeft + 10;
+      image.style.marginLeft= marginLeft +'px';
+  }
+  var interval=setInterval(moveRight,100);
     
 };
