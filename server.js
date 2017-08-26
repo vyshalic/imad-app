5196ff0027fd55e5a70c4c4684e1bd1d32cb21d7
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-26 11:59:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+
+var config={
+    user: 'vyshalichandramohan',
+    database:'vyshalichandramohan',
+    host:'db.app.imad.hasura.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -76,8 +85,19 @@ function createTemplate(data){
     `;
     return htmlTemplate;
     }
-
-
+    
+var pool=new Pool(config);
+app.get('/test-db', function (req, res) {
+  //make a select request
+  //return the request with a response
+  pool.query('SELECT *FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
