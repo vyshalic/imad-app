ee5987729e6acf7492482637f4518045f52a2d54
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-26 12:06:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Pool= require('pg').Pool;
 var config={
     user: 'vyshalichandramohan',
     database:'vyshalichandramohan',
-    host:'db.app.imad.hasura.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
