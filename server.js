7a18e855586ebe42c92c465af108782a28a1478b
vyshalic <30628995+vyshalic@users.noreply.github.com>
2017-08-26 19:53:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,11 +102,26 @@ app.get('/test-db', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function(req, res){
+app.get('/articles/:articleName',function(req, res){
     //By putting a colon infront , the server tries to match the name
     //passing parameter as articles[articleName] to match the url name and get file
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName,function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          if(result.rows.length===0){
+              res.status(404).send('Article not found!');
+          }else{
+        
+         var articleData= result.rows[0];
+         res.send(createTemplate(articleData));
+              
+          }
+      }
+  });
+    
 });
 var counter = 0;
 app.get('/counter', function(req,res) {
